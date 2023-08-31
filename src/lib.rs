d76e7b4fe211474d858d0dec8201f51f8f40dba4
Johanna Fransson <adrina0002@gmail.com>
2023-08-31 14:09:11 -0400

    add new syntax

--- src/lib.rs
+++ src/lib.rs
@@ -5,61 +5,62 @@ use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let name = "everyname.eth"; // Replace with the name you're querying
-    let address = fetch_everyname_address(name).await?;
-    println!("Address: {:?}", address);
+    dotenv().ok(); // Load environment variables from .env file
 
-    let address = "0xD475C94562cabC52e38cb3012D976185813284bb"; // Replace with the address you're querying
-    let name = fetch_everyname_name(address).await?;
-    println!("Name: {:?}", name);
+    if let Some(api_key) = std::env::var("NEXT_PUBLIC_EVERYNAME_API_KEY").ok() {
+        let name = "example"; // Replace with the name you're querying
+        let address = fetch_everyname_address(&name, &api_key).await?;
+        let name = fetch_everyname_name(&address, &api_key).await?;
+        println!("Name: {:?}", name);
+        println!("Address: {:?}", address);
+    } else {
+        println!("API key not set.");
+    }
 
     Ok(())
 }
 
 
+async fn fetch_everyname_address(
+    name: &str,
+    api_key: &str,
+) -> Result<Option<String>, reqwest::Error> {
+    let url = format!(
+        "https://api.everyname.xyz/forward?domain={}",
+        reqwest::to_url_encoded(name)
+    );
 
-async fn fetch_everyname_address(name: &str) -> Result<Option<String>, reqwest::Error> {
-    if let Some(api_key) = std::env::var("EVERYNAME_API_KEY").ok() {
-        let url = format!(
-            "https://api.everyname.xyz/forward?domain={}",
-            reqwest::to_url_encoded(name)
-        );
+    let response = reqwest::Client::new()
+        .get(&url)
+        .header("Accept", "application/json")
+        .header("api-key", api_key)
+        .send()
+        .await?;
 
-        let response = reqwest::Client::new()
-            .get(&url)
-            .header("Accept", "application/json")
-            .header("api-key", api_key)
-            .send()
-            .await?;
+    let data: serde_json::Value = response.json().await?;
+    let address = data["address"].as_str().map(|s| s.to_string());
 
-        let data: serde_json::Value = response.json().await?;
-        let address = data["address"].as_str().map(|s| s.to_string());
-
-        Ok(address)
-    } else {
-        Ok(None)
-    }
+    Ok(address)
 }
 
-async fn fetch_everyname_name(address: &str) -> Result<Option<String>, reqwest::Error> {
-    if let Some(api_key) = std::env::var("EVERYNAME_API_KEY").ok() {
-        let url = format!(
-            "https://api.everyname.xyz/reverse?address={}&network=eth",
-            reqwest::to_url_encoded(address)
-        );
+async fn fetch_everyname_name(
+    address: &str,
+    api_key: &str,
+) -> Result<Option<String>, reqwest::Error> {
+    let url = format!(
+        "https://api.everyname.xyz/reverse?address={}&network=eth",
+        reqwest::to_url_encoded(address)
+    );
 
-        let response = reqwest::Client::new()
-            .get(&url)
-            .header("Accept", "application/json")
-            .header("api-key", api_key)
-            .send()
-            .await?;
+    let response = reqwest::Client::new()
+        .get(&url)
+        .header("Accept", "application/json")
+        .header("api-key", api_key)
+        .send()
+        .await?;
 
-        let data: serde_json::Value = response.json().await?;
-        let name = data["domain"].as_str().map(|s| s.to_string());
+    let data: serde_json::Value = response.json().await?;
+    let name = data["domain"].as_str().map(|s| s.to_string());
 
-        Ok(name)
-    } else {
-        Ok(None)
-    }
+    Ok(name)
 }
